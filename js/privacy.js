c43a90f448779453ea170aa0ecf64da3adb8450d
Murugadasan <avenkata.india.2026@gmail.com>
2026-05-16 12:09:02 +0530

    Delete js/privacy.js
    Legacy file deleted

--- js/privacy.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") ; // TODO replace with close page
-});
-
-window.addEventListener('load', () => {
-    if (typeof initRippleEngine === 'function') initRippleEngine();
-});
-
